b0d8e7dd36d044ddc2cd47416a10ddef0b28a095
VictorS <victorsantos12dima@gmail.com>
2025-08-18 20:44:32 -0300

    First commit: first files

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from "path";
+import { fileURLToPath } from "node:url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+export default defineConfig([
+  { files: ['**/*.js'], plugins: { js }, extends: ['js/recommended'] },
+  ...compat.extends("airbnb"),
+  ...compat.config({ rules: { "no-console": "off",
+    "class-methods-use-this": "off",
+   } }),
+]);
+
+
+
---
+++ src/controllers/HomeController.js
@@ -0,0 +1,7 @@
+class HomeController {
+  index(req, res) {
+    res.json({ tudoCerto: true });
+  }
+}
+
+export default new HomeController();
