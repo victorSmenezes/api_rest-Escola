78b775ced092ba63a53b04d33e47803365e3af18
VictorS <victorsantos12dima@gmail.com>
2025-08-22 20:21:10 -0300

    feat: add new route / migration to unique email

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -2,16 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Victor',
-      sobrenome: 'Santos',
-      email: 'victor@santos.com',
-      idade: 42,
-      peso: 90,
-      altura: 1.9,
-    });
-
-    res.json(novoAluno);
+    res.json();
   }
 }
 
---
+++ src/database/migrations/20250822231406-mudar-email-aluno-unique.js
@@ -0,0 +1,8 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+  },
+
+  async down() {},
+};
