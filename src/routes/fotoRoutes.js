c7f6d626eb5fc5db96aaa85bbd00910f08ca90b6
VictorS <victorsantos12dima@gmail.com>
2025-08-26 15:01:06 -0300

    feat: add multer and route to take foto

---
+++ src/routes/fotoRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import multer from 'multer';
+import fotoController from '../controllers/fotoController';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig);
+const router = new Router();
+
+router.post('/', upload.single('foto'), fotoController.store);
+
+export default router;
