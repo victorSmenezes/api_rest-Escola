d0e8b0821ae99fedb382f52f8ddda548173cd3c9
VictorS <victorsantos12dima@gmail.com>
2025-08-25 21:33:18 -0300

    feat: CRUD to alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -6,6 +6,102 @@ class AlunoController {
 
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(404).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.json(alunoAtualizado);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(404).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({
+        apagado: true,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(404).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import alunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', loginRequired, alunoController.store);
+router.put('/:id', loginRequired, alunoController.update);
+router.delete('/:id', loginRequired, alunoController.delete);
+router.get('/:id', alunoController.show);
 
 export default router;
